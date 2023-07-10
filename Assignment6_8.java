a2c8a4c9b198d5eeb1aa70f6fd40d39706c70fbf
Rohit Kushwaha <113577083+Rohitkushwaha7389@users.noreply.github.com>
2023-07-10 11:39:45 +0530

    Create  Assignment6_8.java

---
+++ Assignment6_8.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Assignment6_8 { 
+
+
+        public static int[][] multiply(int[][] M1, int[][] M2) {
+            int rowsA = M1.length;
+            int colsA = M1[0].length;
+            int colsB = M2[0].length;
+    
+            int[][] finalM = new int[rowsA][colsB];
+    
+            for (int i = 0; i < rowsA; i++) {
+                for (int j = 0; j < colsB; j++) {
+                    for (int k = 0; k < colsA; k++) {
+                        finalM[i][j] += M1[i][k] * M2[k][j];
+                    }
+                }
+            }
+    
+            return finalM;
+        }
+
+public static void main(String[] args) {
+            int[][] M1 = {
+                {1 ,0,0},
+                {-1,0,0}
+            };
+    
+            int[][] M2 = {
+                {7,0,0},
+                {0,0,0},
+                {0,0,1}
+            };
+    
+            int[][] finalMM = multiply(M1, M2);
+    
+            // Print the finalMMing matrix
+            for (int[] row : finalMM) {
+                for (int element : row) {
+                    System.out.print(element + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
